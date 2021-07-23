a9a7d730c95ccc986199b7b97343cef132bc4990
ginaserakalala <sserakal@student.wethinkcode.co.za>
2021-07-23 09:33:12 +0200

    initial commit for kf project

---
+++ src/sign-in.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import AppTheme from './AppTheme'
+import SignIn from './sign-in';
+
+export default function Page() {
+  return (
+    <AppTheme>
+      <SignIn />
+    </AppTheme>
+  );
+}
