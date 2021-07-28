53e25d2e4fd16efd814ff744e3d968081986bc38
ginaserakalala <sserakal@student.wethinkcode.co.za>
2021-07-28 09:56:16 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import Header from "./Header";
 //import "./styles.css";
 //import "../build/css/style.css";
 
-import * as firebase from "firebase";
+//import * as firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
 import firebaseConfig from "./firebase.config";
 
 firebase.initializeApp(firebaseConfig);
