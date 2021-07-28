a4f932f4988824876fe42fb77b804fae3b357ea4
ginaserakalala <sserakal@student.wethinkcode.co.za>
2021-07-28 10:21:16 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import ReactDOM from "react-dom";
