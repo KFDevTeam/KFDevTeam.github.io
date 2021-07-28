cd8fd562eb68367f93c3392d6581eafbf20ec68a
ginaserakalala <sserakal@student.wethinkcode.co.za>
2021-07-28 10:36:35 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -1,55 +1,15 @@
-import React, { useState } from "react";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import ReactDOM from "react-dom";
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
-import routes from "./routes.js";
-import Header from "./Header";
-//import "./styles.css";
-//import "../build/css/style.css";
-
-//import * as firebase from "firebase";
-import firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from "./firebase.config";
-
-firebase.initializeApp(firebaseConfig);
-
-
-export const AuthContext = React.createContext(null);
-
-//function App() {
-  const [isLoggedIn, setLoggedIn] = useState(false);
-
-
-  return (
-    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
-      Is logged in? {JSON.stringify(isLoggedIn)}
-      <div className="App">
-        <Router>
-          <Header />
-
-          <Switch>
-            {routes.map(route => (
-              <Route
-                key={route.path}
-                path={route.path}
-                exact={route.exact}
-                component={route.main}
-              />
-            ))}
-          </Switch>
-        </Router>
-      </div>
-    </AuthContext.Provider>
-  );
-//}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 
 
