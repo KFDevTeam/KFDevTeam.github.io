ad8212956a4933d9d6c770af2e63013deeb59d8b
ginaserakalala <sserakal@student.wethinkcode.co.za>
2021-07-29 08:02:31 +0200

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Redirect } from 'react-router'
-
+import { useHistory } from "react-router-dom";
+       nlknknjkn /jbnljB M<nk lnk;lnm;kljm 
+i[p 0-]0=-p=n o poipoiopiopikpoikpoikpoujpo9upub kjn.b /kn nkb nb jhgb uhu/,;lm;kn   jlh 90uu90u9 u99u9opimoioikoikpoipoipipipioiiiiiiiiiiiiiiipoiopuhhhgtt77 bt78 t7tt7t78t9t79-8 989y98988 y8y8y89y98y89y89y8 89y 79 t7tt78t7tt8t 8tt t t6tr76 r7r7r76rt6t8 y78y 7y 89y978y 89y89y80[o y8
 import { Navigation } from './components/navigation'
 import { Header } from './components/header'
 import { Features } from './components/features'
@@ -14,12 +16,22 @@ import { Contact } from './components/contact'
 import JsonData from './data/data.json'
 import SmoothScroll from 'smooth-scroll'
 
+import firebase from 'firebase/app';
+import firebaseConfig from "./firebase.config";
+
+firebase.initializeApp(firebaseConfig);
+export const AuthContext = React.createContext(null);
+
+
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
   speedAsDuration: true,
 })
 
 const App = () => {
+
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
   const [landingPageData, setLandingPageData] = useState({})
   useEffect(() => {
     setLandingPageData(JsonData)
@@ -37,7 +49,23 @@ const App = () => {
       <Team data={landingPageData.Team} />
       <Contact data={landingPageData.Contact} />
     </div>
+
+
+
+
+
   )
+
+
+
+
+  
+
+
+
+  
+
+
 }
 
 export default App
--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -23,7 +23,7 @@ export const Gallery = (props) => {
                       <h4>View Report</h4>
                     </div>
                     <img
-                      src='img/portfolio/01-small.jpg'
+                      src='img/portfolio/01-large.jpg'
                       className='img-responsive'
                       alt='Project Title'
                     />{' '}
@@ -36,7 +36,7 @@ export const Gallery = (props) => {
                 <div className='hover-bg'>
                   {' '}
                   <a
-                    href='img/portfolio/02-large.jpg'
+                    href='img/portfolio/01-large.jpg'
                     title='Project Title'
                     data-lightbox-gallery='gallery1'
                   >
@@ -44,7 +44,7 @@ export const Gallery = (props) => {
                       <h4>View Report</h4>
                     </div>
                     <img
-                      src='img/portfolio/02-small.jpg'
+                      src='img/portfolio/01-large.jpg'
                       className='img-responsive'
                       alt='Project Title'
                     />{' '}
@@ -57,7 +57,7 @@ export const Gallery = (props) => {
                 <div className='hover-bg'>
                   {' '}
                   <a
-                    href='img/portfolio/03-large.jpg'
+                    href='img/portfolio/01-large.jpg'
                     title='Project Title'
                     data-lightbox-gallery='gallery1'
                   >
@@ -65,7 +65,7 @@ export const Gallery = (props) => {
                       <h4>View Report</h4>
                     </div>
                     <img
-                      src='img/portfolio/03-small.jpg'
+                      src='img/portfolio/01-large.jpg'
                       className='img-responsive'
                       alt='Project Title'
                     />{' '}
