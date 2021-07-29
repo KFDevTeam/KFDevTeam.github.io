8e94c78aeeaf698a33863b59c31d70497368eea9
ginaserakalala <sserakal@student.wethinkcode.co.za>
2021-07-29 09:05:49 +0200

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Redirect } from 'react-router'
 import { useHistory } from "react-router-dom";
